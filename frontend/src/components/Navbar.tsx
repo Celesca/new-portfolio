3d19203da6368e47f62e510227b32cb5a6c6b643
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-07-01 01:48:01 +0700

    init: project

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="ps-8 pt-2">
+            <h1 className="text-2xl pt-4">Celescadev</h1>
+        </div>
+        <div>
+            <ul className="flex justify-end pt-4 pr-8">
+                <li className="pr-4">
+                    <a href="https://celescadev.com" className="hover:text-blue-500">Home</a>
+                </li>
+                <li className="pr-4">
+                    <a href="https://celescadev.com/about" className="hover:text-blue-500">About</a>
+                </li>
+                <li className="pr-4">
+                    <a href="https://celescadev.com/contact" className="hover:text-blue-500">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
