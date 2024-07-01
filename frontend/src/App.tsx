f772a755f6cb6cdfe0a524d9d86c2fff25cd4643
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-07-01 13:22:36 +0700

    add: image

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import HomePage from './HomePage/HomePage';
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <HomePage/>
+      
     </>
   )
 }
 
-export default App
+export default App;
---
+++ frontend/src/HomePage/HomePage.tsx
@@ -0,0 +1,16 @@
+const HomePage = () => {
+  return (
+    <div>
+        <div>
+            <img src="yoimiya.jpg" alt="yoimya" width="300" height="300" className="" />
+        </div>
+
+        <div>
+            <p>My name is Celeste. I am a full stack developer.</p>
+        </div>
+       
+    </div>
+  )
+}
+
+export default HomePage
