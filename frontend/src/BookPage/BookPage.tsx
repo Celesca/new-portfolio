9f96c5e70a7b8dd3cf3c88826ffc9c4c00dea6d4
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-07-05 13:36:16 +0700

    add: BookPage

---
+++ frontend/src/BookPage/BookPage.tsx
@@ -0,0 +1,38 @@
+import { Layout, theme } from 'antd';
+import { Content, Footer, Header } from 'antd/es/layout/layout';
+import { UsbFilled } from '@ant-design/icons';
+
+const BookPage: React.FC = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  
+  return (
+    <Layout>
+    <Header className="text-2xl pl-4 pt-6 font-bold text-amber-400 bg-gray-300">
+    <div>
+      <UsbFilled className="pr-4"/>
+        <span>Hello World</span>
+      </div>
+      </Header>
+
+    <Content style={{ margin: '0 16px' }}>
+      <div
+        style={{
+          padding: 24,
+          minHeight: 360,
+          background: colorBgContainer,
+          borderRadius: borderRadiusLG,
+        }}
+      >
+        Bill is a cat.
+      </div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>
+      Ant Design ©{new Date().getFullYear()} Created by Ant UED
+    </Footer>
+  </Layout>
+  )
+}
+
+export default BookPage
