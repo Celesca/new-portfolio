1e3b3de9805ca2cbb9df1151cea11d2e045e33eb
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-07-05 14:12:33 +0700

    add: cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,14 @@ import bodyParser from 'body-parser';
 import { bookRouter } from './bookRouter';
 import express, { Request, Response, Application } from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app: Application = express();
 const port: number = 3000;
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
