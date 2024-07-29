095580df2bc9849aae2ecef1c0cb67d6e756d371
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-07-30 00:06:29 +0700

    add: starback.js

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -4,6 +4,7 @@ import { Footer } from 'antd/es/layout/layout';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
+import Starback from 'starback';
 
 const HomePage: React.FC = () => {
   
@@ -13,14 +14,14 @@ const HomePage: React.FC = () => {
 
   return (
     <Layout className="homepage-container">
-      <div className="flex header-container">
+      <div className="flex header-container pt-16">
         <div className="flex flex-col items-center p-16 w-full">
           <img src="https://i.redd.it/35j9y5v43ro71.png" alt="homepage" 
           className="homepage-image rounded-full w-64"/>
         </div>
         <div className="w-full">
           <div className="text-container flex flex-col mt-32">
-            <h1 className="text-4xl my-4" data-aos="fade-up">Hello. I'm <span className="font-bold">Celesca. </span> </h1>
+            <h1 className="text-4xl text-white my-4" data-aos="fade-up">Hello. I'm <span className="font-bold">Celesca. </span> </h1>
             <h2 className="text-lg my-4">I love to do some researching about programming and others</h2>
           </div>
         </div>
