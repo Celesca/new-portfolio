fd09556462bfc21f4958aeb0f78207a2b5eb9af4
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-07-29 23:52:06 +0700

    change navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -9,13 +9,13 @@ const Navbar = () => {
         <div>
             <ul className="flex justify-end pt-4 pr-8">
                 <li className="pr-4">
-                    <a href="/" className="hover:text-blue-500 pr-8">Home</a>
+                    <a href="/" className="hover:text-blue-500 text-white pr-8 nav-text">Home</a>
                 </li>
                 <li className="pr-4">
-                    <a href="/book" className="hover:text-blue-500 pr-8">Book</a>
+                    <a href="/book" className="hover:text-blue-500 text-white pr-8">Book</a>
                 </li>
                 <li className="pr-4">
-                    <a href="/contact" className="hover:text-blue-500">Contact</a>
+                    <a href="/contact" className="hover:text-blue-500 text-white ">Contact</a>
                 </li>
             </ul>
         </div>
