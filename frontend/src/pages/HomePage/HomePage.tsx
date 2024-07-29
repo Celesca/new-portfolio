b530d3bfcf336817d01f977563c957629ce32455
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-07-29 23:40:53 +0700

    add: AOS

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,16 @@
 import { Layout } from 'antd';
 import './HomePage.css';
 import { Footer } from 'antd/es/layout/layout';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 const HomePage: React.FC = () => {
   
+  useEffect(() => {
+    AOS.init();
+  }, [])
+
   return (
     <Layout className="homepage-container">
       <div className="flex header-container">
@@ -12,8 +19,8 @@ const HomePage: React.FC = () => {
           className="homepage-image rounded-full w-64"/>
         </div>
         <div className="w-full">
-          <div className="text-container flex flex-col mt-12">
-            <h1 className="text-4xl my-4" data-aos="fade-up">Hello. I'm Celesca.</h1>
+          <div className="text-container flex flex-col mt-32">
+            <h1 className="text-4xl my-4" data-aos="fade-up">Hello. I'm <span className="font-bold">Celesca. </span> </h1>
             <h2 className="text-lg my-4">I love to do some researching about programming and others</h2>
           </div>
         </div>
