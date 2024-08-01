7fadf7c2536b4b302d81fe497bc151095c04e11b
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-08-01 11:49:53 +0700

    add: heart icon:

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -20,6 +20,7 @@ const HomePage: React.FC = () => {
         </div>
         <div className="w-full">
           <div className="text-container flex flex-col mt-32">
+            <img src="love-always-wins.png" width="50"></img>
             <h1 className="text-4xl text-white my-4" data-aos="fade-up">Hello. I'm <span className="font-bold">Celesca. </span> </h1>
             <h2 className="text-lg my-4">I love to do some researching about programming and others</h2>
           </div>
