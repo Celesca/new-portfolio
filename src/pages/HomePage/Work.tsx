ff32e19406e57a93adad966cbe426d65d168c8de
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-08-04 22:49:09 +0700

    add: data-aos for aboutme

--- src/pages/HomePage/Work.tsx
+++ src/pages/HomePage/Work.tsx
@@ -5,24 +5,7 @@ const Work = () => {
 
     <div className="mt-48 flex flex-col items-center justify-center">
         <h1 className="text-white text-4xl my-24">My Works</h1>
-        <section className="nes-container w-4/6 flex justify-center items-center is-dark">
-            <section className="message-list">
-                <section className="message -right">
-                    <div className="nes-balloon from-right is-dark">
-                    <p>So.. Show me your experiences and competitions</p>
-                    </div>
-                    <i className="nes-bcrikko"></i>
-                </section>
-
-                <section className="message -left">
-                    <i className="nes-bcrikko"></i>
-                    <div className="nes-balloon from-left is-dark">
-                    <p>Sure!! Let's go </p>
-                    </div>
-                    
-                </section>
-            </section>
-        </section>
+       
 
         </div>
     )
