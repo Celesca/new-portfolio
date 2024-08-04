4ea38362eba09ccade4a21f9f25d21002475eee0
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-08-04 19:12:56 +0700

    add: work page

--- src/pages/HomePage/Work.tsx
+++ src/pages/HomePage/Work.tsx
@@ -1,23 +1,25 @@
 const Work = () => {
 
     return (
+        
 
-    <div>
-        <section className="nes-container is-dark">
-        <section className="message-list">
-            <section className="message -left">
-                <i className="nes-bcrikko"></i>
-                <div className="nes-balloon from-left is-dark">
-                <p>Hello NES.css</p>
-                </div>
-            </section>
+    <div className="flex flex-col items-center justify-center">
+        <h1>My Works</h1>
+        <section className="nes-container w-4/6 flex justify-center items-center is-dark">
+            <section className="message-list">
+                <section className="message -left">
+                    <i className="nes-bcrikko"></i>
+                    <div className="nes-balloon from-left is-dark">
+                    <p>Hello NES.css</p>
+                    </div>
+                </section>
 
-            <section className="message -right">
-                <div className="nes-balloon from-right is-dark">
-                <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
-                </div>
-                <i className="nes-bcrikko"></i>
-            </section>
+                <section className="message -right">
+                    <div className="nes-balloon from-right is-dark">
+                    <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+                    </div>
+                    <i className="nes-bcrikko"></i>
+                </section>
             </section>
         </section>
 
