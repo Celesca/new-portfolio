9b26410ee47957a6360c974fff40e886b58d9a17
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-08-04 22:44:05 +0700

    add: social media

--- src/pages/HomePage/Work.tsx
+++ src/pages/HomePage/Work.tsx
@@ -3,22 +3,23 @@ const Work = () => {
     return (
         
 
-    <div className="flex flex-col items-center justify-center">
-        <h1>My Works</h1>
+    <div className="mt-48 flex flex-col items-center justify-center">
+        <h1 className="text-white text-4xl my-24">My Works</h1>
         <section className="nes-container w-4/6 flex justify-center items-center is-dark">
             <section className="message-list">
+                <section className="message -right">
+                    <div className="nes-balloon from-right is-dark">
+                    <p>So.. Show me your experiences and competitions</p>
+                    </div>
+                    <i className="nes-bcrikko"></i>
+                </section>
+
                 <section className="message -left">
                     <i className="nes-bcrikko"></i>
                     <div className="nes-balloon from-left is-dark">
-                    <p>Hello NES.css</p>
+                    <p>Sure!! Let's go </p>
                     </div>
-                </section>
-
-                <section className="message -right">
-                    <div className="nes-balloon from-right is-dark">
-                    <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
-                    </div>
-                    <i className="nes-bcrikko"></i>
+                    
                 </section>
             </section>
         </section>
