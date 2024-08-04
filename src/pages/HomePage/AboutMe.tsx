c1298ce3c36f0e55685e34092b5c38cd13295651
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-08-04 23:32:13 +0700

    add: carousel

--- src/pages/HomePage/AboutMe.tsx
+++ src/pages/HomePage/AboutMe.tsx
@@ -6,8 +6,8 @@ const AboutMe = () => {
     <>
         <div className="flex flex-col items-center py-24">
             <h1 className="text-white text-4xl my-24" data-aos="zoom-in-up">About me</h1>
-            <i className="nes-mario" data-aos="zoom-in-up"></i>
-            <div data-aos="fade-right" className="nes-container is-dark with-title flex flex-col lg:flex-row justify-between items-center lg:justify-around w-4/6">
+            <i className="nes-mario" data-aos="zoom-in-up" data-aos-duration="700"></i>
+            <div data-aos="fade-right" data-aos-duration="1000" className="nes-container is-dark with-title flex flex-col lg:flex-row justify-between items-center lg:justify-around w-4/6">
                 <img src="https://stickershop.line-scdn.net/stickershop/v1/product/13505512/LINEStorePC/main.png?v=1" alt="homepage" className="w-1/2 h-1/3 lg:w-1/4 lg:h-1/3"></img>
                 <div className="lists lg:w-1/2">
                     <ul className="list-disc leading-9">
--- src/pages/HomePage/Work.tsx
+++ src/pages/HomePage/Work.tsx
@@ -1,3 +1,5 @@
+import Carousel from "../../components/Carousel"
+
 const Work = () => {
 
     return (
@@ -5,6 +7,8 @@ const Work = () => {
 
     <div className="mt-48 flex flex-col items-center justify-center">
         <h1 className="text-white text-4xl my-24">My Works</h1>
+
+        <Carousel />
        
 
         </div>
