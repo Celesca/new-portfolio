96b79a228939b09cba1c295a9bb1a5ac5a3b9b5d
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-08-04 14:34:48 +0700

    fix: about me

--- frontend/src/pages/HomePage/AboutMe.tsx
+++ frontend/src/pages/HomePage/AboutMe.tsx
@@ -7,13 +7,13 @@ const AboutMe = () => {
         <div className="flex flex-col items-center py-24">
             <h1 className="text-white text-2xl">About me</h1>
             <div className="nes-container is-dark with-title flex justify-around w-1/2">
-                <img src="https://i.redd.it/35j9y5v43ro71.png" alt="homepage" width="200"></img>
-                <div className=" w-3/5">
-                    <ol>
-                        <p className="title">Sawit Koseeyaumporn (Folk)</p>
-                        <p>Computer Engineering, KMUTT</p>
-                        <p>Beta Microsoft Learn Student Ambassador</p>
-                    </ol>
+                <img src="https://stickershop.line-scdn.net/stickershop/v1/product/13505512/LINEStorePC/main.png?v=1" alt="homepage" width="200"></img>
+                <div className="">
+                    <ul className="list-disc leading-9">
+                        <li className="title">Sawit Koseeyaumporn (Folk)</li>
+                        <li>Computer Engineering, <span className="text-orange-500">KMUTT</span></li>
+                        <li><span className="text-blue-600">Beta</span> Microsoft Learn Student Ambassador</li>
+                    </ul>
                     
                 </div>
             </div>
