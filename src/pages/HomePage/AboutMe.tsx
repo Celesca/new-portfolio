dd589f715ba2276095a0ea62dee6579dbf99556a
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-08-04 18:09:17 +0700

    add: mario

--- src/pages/HomePage/AboutMe.tsx
+++ src/pages/HomePage/AboutMe.tsx
@@ -6,6 +6,7 @@ const AboutMe = () => {
     <>
         <div className="flex flex-col items-center py-24">
             <h1 className="text-white text-4xl my-24">About me</h1>
+            <i className="nes-mario"></i>
             <div className="nes-container is-dark with-title flex flex-col lg:flex-row justify-between items-center lg:justify-around w-4/6">
                 <img src="https://stickershop.line-scdn.net/stickershop/v1/product/13505512/LINEStorePC/main.png?v=1" alt="homepage" className="w-1/2 h-1/3 lg:w-1/4 lg:h-1/3"></img>
                 <div className="lists lg:w-1/2">
