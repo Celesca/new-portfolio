d73831c3d8b7a3634a069e3f56a4aa3db20af8cf
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-08-05 00:38:30 +0700

    add: carousel image

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -5,7 +5,7 @@ const Carousel = () => {
         <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
 
             <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="/docs/images/carousel/carousel-1.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                <img src="" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
             </div>
 
             <div className="hidden duration-700 ease-in-out" data-carousel-item>
