be246d90ad40922e46eda02ed4f827efcb6d3cb2
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-12-15 12:26:33 +0700

    add: TypeAnimation

--- src/pages/HomePage/Header.tsx
+++ src/pages/HomePage/Header.tsx
@@ -1,6 +1,7 @@
 import 'nes.css/css/nes.min.css';
 import './Header.css';
 import './HomePage.css'
+import { TypeAnimation } from 'react-type-animation';
 
 const Header = () => {
 
@@ -22,7 +23,25 @@ const Header = () => {
     <div className="w-full">
       <div className="flex flex-col items-center lg:items-start lg:mt-32">
         <img src="love-always-wins.png" data-aos="fade-up" width="50"></img>
-        <h1 className="md:text-xl sm:text-md text-white my-4" data-aos="fade-up" data-aos-duration="700">Hello. I'm <span className="font-bold text-green-400">Celesca. </span> </h1>
+        <h1 className="md:text-xl sm:text-md text-white my-4" data-aos="fade-up" data-aos-duration="700">Hello. My name is <span className="font-bold text-green-400">Celesca. </span> </h1>
+        <span className="md:text-xl sm:text-md text-white my-4" data-aos="fade-up" data-aos-duration="700">I'm &nbsp;
+        <TypeAnimation
+            sequence={[
+              'AI Engineer.', 
+              3000, 
+              'Entrepreneur.',
+              3000,
+              'Software Engineer.',
+              3000,
+              'Hackathon Inspirer.'
+            ]}
+            wrapper="span"
+            cursor={true}
+            repeat={Infinity}
+            className="md:text-xl sm:text-md text-white"
+          />
+        </span>
+        
         <h2 className="md:text-md sm:text-sm text-white mt-4 mb-8" data-aos="fade-up" data-aos-duration="1000">Computer Engineering, <span className="font-bold text-orange-500">KMUTT</span></h2>
           <button type="button" data-aos="fade-up"
               data-aos-duration="1300" className="nes-btn is-primary w-48"
