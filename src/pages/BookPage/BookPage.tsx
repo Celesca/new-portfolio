1faaac11dc87d3c3b14beed740ccfe80ee28393f
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-12-22 11:18:45 +0700

    add: BookPage.tsx

--- src/pages/BookPage/BookPage.tsx
+++ src/pages/BookPage/BookPage.tsx
@@ -1,9 +1,31 @@
+import { Layout } from 'antd';
+import '../HomePage/HomePage.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+import FooterSection from '../../components/FooterSection';
+
 const BookPage: React.FC = () => {
+  
+  useEffect(() => {
+    AOS.init();
+  }, [])
 
   return (
-    <div>
-      <h1>Book Page</h1>
+    <Layout className="homepage-container">
+
+    <div className="night" >
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
     </div>
-  );
+
+
+    <FooterSection />
+  </Layout>
+  )
 }
-export default BookPage;
+
+export default BookPage
